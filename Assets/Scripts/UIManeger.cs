41f49ce6d094c70abd44bdf36eca8cdd9575c277
kamera <asahi20000124@icloud.com>
2018-08-29 10:39:17 +0900

    UIManegerを追加、GameUIのCanvasをPrefab化

---
+++ Assets/Scripts/UIManeger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManeger : MonoBehaviour {
+    [SerializeField]
+    private Slider healthSlider;
+    [SerializeField]
+    private Text scoreText;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //ScoreのUpdate
+    void UpdateScore(int score) {
+        scoreText.text = "Score : " + score.ToString();
+
+    }
+
+    //PlayerのHP/MaxHPを代入
+    void UpdateHealthBar(int playerHealth,int maxHealth) {
+        healthSlider.value = playerHealth / maxHealth;
+    }
+
+}
